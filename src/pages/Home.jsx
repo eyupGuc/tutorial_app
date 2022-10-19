4d60990b88979240c3227b8085939fd071c69982
eyupGuc <meydanguresi@gmail.com>
2022-10-19 07:38:12 +0300

    data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,21 @@
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
+import axios  from "axios";
+import { useEffect, useState } from "react"; 
 
 const Home = () => {
+  const [tutorials,setTutorials]=useState([])
+  const url= "https://axios-example-cw.herokuapp.com/api/tutorials";
+  const getTutorials=async()=>{
+   const {data}= await axios(url);
+   console.log(data)
+  //  setTutorials(data) // Burda çağrılırsa render/state sonsuz döngüye giriyor
+  }
+  // getTutorials() // Eğer state olsa idi sürekli render olurdu.
+  useEffect(()=>{
+    getTutorials()
+  },[])
+
   return (
     <>
       <AddTutorial />
